c82015d49dcb4d1feb616ff5437ca387b70384b2
Mathias Gilson <mathias.gilson@outlook.com>
2023-04-04 19:56:00 +0100

    portal sigging

---
+++ nightwatch/uploadMonthlyInvoice.ts
@@ -0,0 +1,16 @@
+import { NightwatchTests } from "nightwatch"
+
+export default {
+    "Login": () => {
+        return browser
+            .url('https://www.acesso.gov.pt/v2/loginForm?partID=PFAP&path=/geral/dashboard')
+            .waitForElementVisible('body')
+            .click('#content-area > div > div > label:nth-child(4) > span')
+            .setValue('#username', [process.env.PORTAL_USERNAME])
+            .click('#password-nif')
+            .setValue('#password-nif', [process.env.PORTAL_PASSWORD])
+            .click('#sbmtLogin')
+            .waitForElementNotPresent('body', 1000)
+            .end();
+    }
+}
